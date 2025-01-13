ee250e94f7b978cae6e8393965c5b8487ef17b7b
Frekara <daivakautharr@gmail.com>
2025-01-13 22:05:10 +0700

    gak tau

--- frontend/src/pages/dashboard/Kategori.jsx
+++ frontend/src/pages/dashboard/Kategori.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { URL_KATEGORI } from '../../utils/Endpoint';
+import { FaCirclePlus, FaPencil, FaRegTrashCan } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import '../../style.css';
+import {Button, Image, ConfigProvider } from 'antd';
 
 const Kategori = () => {
     const [kategori, setKategori] = useState([]);
@@ -13,7 +17,9 @@ const Kategori = () => {
         try {
             const response = await axios.get(URL_KATEGORI);
             setKategori(response.data);
-            const filteredKategori = response.data.map(({ _id, ...rest }) => rest);
+            const filteredKategori = response.data.map(item => ({
+                ...item
+            }));
             setKategori(filteredKategori);
         } catch (error) {
             console.error("Error fetching kategori:", error);
@@ -31,39 +37,68 @@ const Kategori = () => {
     };
 
     return (
+        <div>
+            <div className="flex justify-between">
+                    <h1 className="font-bold mt-1">Daftar Produk Untuk Dikelola</h1>
+                    
+                    <div className="flex">
+                        <Link to={'/dashboard/kategori/create'}>
+                            <Button type="secondary" className="bg-red-800 hover:bg-red-700 text-white font-semibold rounded-3xl h-6 py-4 justify-items-center text-base"><FaCirclePlus /><span className="mb-1">Tambah Kategori</span></Button>
+                        </Link>
+                    </div>
+                </div>
         <div style={{ display: "flex", justifyContent: "center", height: "100vh" }}>
             <Table
-                title="Produk Kategori"
                 headers={["NO", "Nama Kategori", "Aksi"]}
                 data={kategori}
                 onEdit={handleEdit}
                 onDelete={handleDelete}
             />
         </div>
+        </div>
     );
 };
 
 const Table = ({ title, headers, data, onEdit, onDelete }) => (
-    <div style={{ width: "60%", backgroundColor: "#fff", padding: "20px", borderRadius: "10px", textAlign: "center" }}>
-        <h3 className="font-bold mb-5">{title}</h3>
+    <div style={{ width: "90%", backgroundColor: "#fff", padding: "20px", borderRadius: "10px", textAlign: "center" }}>
         <table style={{ width: "100%", borderCollapse: "collapse", margin: "0 auto" }}>
             <thead>
                 <tr>
                     {headers.map((header) => (
-                        <th style={{ border: "1px solid #ddd", background: "#F2E8C6", padding: "8px" }} key={header}>{header}</th>
+                        <th style={{ border: "1px solid #ddd", background: "#F2E8C6", padding: "8px" }} className="font-semibold" key={header}>{header}</th>
                     ))}
                 </tr>
             </thead>
             <tbody>
                 {data.map((item, index) => (
+
                     <tr key={index}>
                         <td style={{ border: "1px solid #ddd", padding: "8px" }}>{index + 1}</td>
-                        {Object.values(item).map((cell, idx) => (
+                        {Object.values(item).slice(1.0).map((cell, idx) => (
                             <td style={{ border: "1px solid #ddd", padding: "8px" }} key={idx}>{cell}</td>
                         ))}
                         <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                            <button onClick={() => onEdit(item.id)} style={{ marginRight: "8px" }}>Edit</button>
-                            <button onClick={() => onDelete(item.id)}>Hapus</button>
+                        <Link to={`/dashboard/kategori/${item?._id}`}>
+                        <Button type="secondary" className="border-2 border-red-800 hover:border-red-600 hover:text-red-700 me-2">
+                            <FaPencil />
+                        </Button>
+                    </Link>
+                    <Button
+                        type="secondary"
+                        className="border-2 border-red-800 hover:border-red-600 hover:text-red-700"
+                        onClick={() => {
+                            console.log('id', item?._id);
+                            axios
+                                .delete(`${URL_KATEGORI}/${item?._id}`)
+                                .then((res) => {
+                                    console.log(res);
+                                    window.location.reload();
+                                })
+                                .catch((err) => console.log('err', err));
+                        }}
+                    >
+                        <FaRegTrashCan />
+                    </Button>
                         </td>
                     </tr>
                 ))}
