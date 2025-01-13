62943628268d27c4dc509ef2f63a4e14b419d04f
Frekara <daivakautharr@gmail.com>
2025-01-13 17:30:00 +0700

    kategori

--- frontend/src/utils/Endpoint.jsx
+++ frontend/src/utils/Endpoint.jsx
@@ -7,3 +7,4 @@ export const URL_VERIFYOTP = `${BASEURL}/api/auth/password/reset/otp`
 export const URL_NEWPASSWORD = `${BASEURL}/api/auth/password/new`
 export const URL_PRODUCT = `${BASEURL}/api/products`
 export const URL_TRANSACTION = `${BASEURL}/api/transactions` 
+export const URL_KATEGORI = `${BASEURL}/api/kategori`
