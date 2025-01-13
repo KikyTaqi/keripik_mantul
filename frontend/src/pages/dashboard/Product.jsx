308b78c844526ec9e25acc99997f1966785fa9b2
KikyTaqi <rifqiramandhani3@gmail.com>
2025-01-13 17:44:40 +0700

    Lanjut styling

--- frontend/src/pages/dashboard/Product.jsx
+++ frontend/src/pages/dashboard/Product.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button, Image } from 'antd';
+import { Table, Button, Image, ConfigProvider } from 'antd';
 import axios from 'axios';
 import { URL_PRODUCT } from '../../utils/Endpoint';
 import { Link } from 'react-router-dom';
 import '../../style.css';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { FaCirclePlus, FaPencil, FaRegTrashCan } from "react-icons/fa6";
 
 const Product = () => {
     const [Products, setProducts] = useState([]);
@@ -30,28 +30,34 @@ const Product = () => {
         {
             title: "NO",
             key: "no",
+            align: "center",
+            width: "5px",
             render: (_, __, index) => index + 1, // row numbering
         },
         {
-            title: 'Name',
+            title: 'Nama Produk',
             dataIndex: 'name',
             key: 'name',
+            align: "center",
         },
         {
             title: 'Stok',
             dataIndex: 'stok',
             key: 'stok',
+            align: "center",
         },
         {
-            title: 'Price',
+            title: 'Harga',
             dataIndex: 'price',
             key: 'price',
+            align: "center",
             render: (price) => `Rp ${price.toLocaleString('id-ID')}`,
         },
         {
             title: 'Kategori',
-            dataIndex: 'kategory',
-            key: 'kategory',
+            dataIndex: 'kategori',
+            key: 'kategori',
+            align: "center",
         },
         {
             title: "Gambar",
@@ -60,19 +66,24 @@ const Product = () => {
                 console.log('recor', record);
                 return <Image src={record?.thumbnail} width={100} loading="lazy" />
             },
+            align: "center",
         },
         {
-            title: 'Action',
+            title: 'Aksi',
             key: 'action',
+            width: "15%",
+            align: "center",
             render: (_, record) => (
-                <>
-                    <Button type="primary" icon={<EditOutlined />}>
-                        <Link to={`/dashboard/products/${record?._id}`}></Link>
-                    </Button>
+                <>  
+                    <Link to={`/dashboard/products/${record?._id}`}>
+                        <Button type="secondary" className="border-2 border-red-800 hover:border-red-600 hover:text-red-700 me-2">
+                            <FaPencil />
+                        </Button>
+                    </Link>
                     <Button
-                        type="primary"
-                        danger
-                        icon={<DeleteOutlined />}
+                        type="secondary"
+                        className="border-2 border-red-800 hover:border-red-600 hover:text-red-700"
+                        loading={loading}
                         onClick={() => {
                             console.log('id', record?._id);
                             axios
@@ -84,6 +95,7 @@ const Product = () => {
                                 .catch((err) => console.log('err', err));
                         }}
                     >
+                        <FaRegTrashCan />
                     </Button>
                 </>
             ),
@@ -91,22 +103,45 @@ const Product = () => {
     ];
 
     return (
-        <div>
-            <h1>List Product</h1>
-            <Link to={'/dashboard/products/create'}>
-                <Button type="primary">Tambah</Button>
-            </Link>
-            <Table
-            dataSource={Products}
-            columns={column}
-            loading="loading"
-            pagination={{ 
-                pageSize: 4, 
-                showSizeChanger: false,
-                className: 'custom-pagination',
+        <ConfigProvider
+            theme={{
+                token: {
+                     // Primary color untuk seluruh aplikasi
+                },
+                components: {
+                    Table: {
+                        headerBg: "#F2E8C6",
+                        headerBorderRadius: 0,
+                        borderColor: "#B9B9B9",
+                    },
+                },
             }}
-            />
-        </div>
+        >
+            <div>
+                <div className="flex justify-between">
+                    <h1 className="font-bold mb-5">Daftar Produk Untuk Dikelola</h1>
+                    
+                    <div className="flex">
+                        <Link to={'/dashboard/products/create'}>
+                            <Button type="secondary" className="bg-red-800 hover:bg-red-700 text-white font-semibold rounded-3xl h-8 py-5 justify-items-center text-base"><FaCirclePlus /> Tambah Produk</Button>
+                        </Link>
+                    </div>
+                </div>
+                
+                <Table
+                dataSource={Products}
+                columns={column}
+                loading="loading"
+                bordered
+                className="mt-4"
+                pagination={{ 
+                    pageSize: 4, 
+                    showSizeChanger: false,
+                    className: 'custom-pagination',
+                }}
+                />
+            </div>
+        </ConfigProvider>
     );
 };
 
