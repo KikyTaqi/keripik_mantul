50e3957767d035dcf48aa6451ede4ade72a2aa6b
KikyTaqi <rifqiramandhani3@gmail.com>
2025-01-13 15:41:58 +0700

    benerin design

--- frontend/src/pages/dashboard/Product.jsx
+++ frontend/src/pages/dashboard/Product.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { URL_PRODUCT } from '../../utils/Endpoint';
 import { Link } from 'react-router-dom';
 import '../../style.css';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const Product = () => {
     const [Products, setProducts] = useState([]);
@@ -27,12 +28,9 @@ const Product = () => {
     // kolom utk tabel
     const column = [
         {
-            title: "Thumbnail",
-            dataIndex: "thumbnail",
-            render: (_, record) => {
-                console.log('recor', record);
-                return <Image src={record?.thumbnail} width={100} loading="lazy" />
-            },
+            title: "NO",
+            key: "no",
+            render: (_, __, index) => index + 1, // row numbering
         },
         {
             title: 'Name',
@@ -40,21 +38,41 @@ const Product = () => {
             key: 'name',
         },
         {
+            title: 'Stok',
+            dataIndex: 'stok',
+            key: 'stok',
+        },
+        {
             title: 'Price',
             dataIndex: 'price',
             key: 'price',
+            render: (price) => `Rp ${price.toLocaleString('id-ID')}`,
+        },
+        {
+            title: 'Kategori',
+            dataIndex: 'kategory',
+            key: 'kategory',
+        },
+        {
+            title: "Gambar",
+            dataIndex: "thumbnail",
+            render: (_, record) => {
+                console.log('recor', record);
+                return <Image src={record?.thumbnail} width={100} loading="lazy" />
+            },
         },
         {
             title: 'Action',
             key: 'action',
             render: (_, record) => (
                 <>
-                    <Button type="primary">
-                        <Link to={`/dashboard/products/${record?._id}`}>Update</Link>
+                    <Button type="primary" icon={<EditOutlined />}>
+                        <Link to={`/dashboard/products/${record?._id}`}></Link>
                     </Button>
                     <Button
                         type="primary"
                         danger
+                        icon={<DeleteOutlined />}
                         onClick={() => {
                             console.log('id', record?._id);
                             axios
@@ -66,7 +84,6 @@ const Product = () => {
                                 .catch((err) => console.log('err', err));
                         }}
                     >
-                        Delete
                     </Button>
                 </>
             ),
