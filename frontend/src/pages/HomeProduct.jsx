ebe28ac80626d9eb79fd8ce13d1b6ea755a463d1
Frekara <daivakautharr@gmail.com>
2025-01-20 09:18:24 +0700

    homeproduct

--- frontend/src/pages/HomeProduct.jsx
+++ frontend/src/pages/HomeProduct.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Card, Col, Row, Button, Typography, message } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
+import { FaRegHeart } from "react-icons/fa"
 import axios from "axios";
 import { URL_PRODUCT } from "../utils/Endpoint";
 import { Link } from "react-router-dom";
@@ -53,138 +54,70 @@ const HomeProduct = () => {
                 <img src={jumbotron_produk} alt="" draggable='false' style={{zIndex:'0', position: 'absolute'}}/>
             </div>
             <div className="py-9 px-10 bg-white relative" style={{zIndex: '5', borderRadius: '80px 80px 0 0'}}>
-                <section id="product-list" className="mb-11">
-                    <Title level={2}>Produk Terlaris</Title>
-                    <div className="bg-[#F2E8C6] p-5">
-                        <Row gutter={[13, 13]}>
-                            {productsTerlaris.slice(0, 4).map((product) => (
-                                <Col span={6} key={product.id}>
-                                    <Card
-                                        style={{
-                                            height: '436px',
-                                            minHeight: '436px',
-                                        }}
-                                        hoverable
-                                        cover={
-                                            <img alt={product.name} src={product.thumbnail} style={{
-                                                maxHeight: '250px',
-                                                overflowY: 'hidden',
-                                                marginBottom: 'auto'
-                                            }}/>
-                                        }>
-                                        <Card.Meta
-                                            style={{
-                                                marginTop: 'auto'
-                                            }}
-                                            title={product.name}
-                                            description={`Rp ${product.price}`}
-                                        />
-                                        <Button
-                                            type="primary"
-                                            icon={<ShoppingCartOutlined />}
-                                            style={{ marginTop: "10px" }}
-                                            // onClick={() => handleAddToCart(product)}
-                                        >
-                                        <Link to={`/checkout/${product._id}`}>Checkout Now</Link>
-                                        </Button>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
-                </section>
-                <section className="mb-11">
-                    <Title level={2}>Direkomendasikan</Title>
-                    <div className="bg-[#F2E8C6] p-5">
-                        <Row gutter={[13, 13]}>
-                            {productsTerlaris.slice(0, 4).map((product) => (
-                                <Col span={6} key={product.id}>
-                                    <Card
-                                        style={{
-                                            height: '436px',
-                                            minHeight: '436px',
-                                        }}
-                                        hoverable
-                                        cover={
-                                            <img alt={product.name} src={product.thumbnail} style={{
-                                                maxHeight: '250px',
-                                                overflowY: 'hidden',
-                                                marginBottom: 'auto'
-                                            }}/>
-                                        }>
-                                        <Card.Meta
-                                            style={{
-                                                marginTop: 'auto'
-                                            }}
-                                            title={product.name}
-                                            description={`Rp ${product.price}`}
-                                        />
-                                        <Button
-                                            type="primary"
-                                            icon={<ShoppingCartOutlined />}
-                                            style={{ marginTop: "10px" }}
-                                            // onClick={() => handleAddToCart(product)}
-                                        >
-                                        <Link to={`/checkout/${product._id}`}>Checkout Now</Link>
-                                        </Button>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
-                </section>
-                <div className="px-4">
-                    <div className="relative" style={{height: '500px', width: '100%'}}>
-                        <img src={keripik3} alt="" className="absolute" style={{
-                            width: '744px',
-                            height: '441px',
-                            borderRadius: '20px 100px 20px 100px',
-                        }}/>
-                        <div className="absolute bottom-1 right-0 transform text-left bg-[#F2E8C6] border-2 border-[#800000]" style={{
-                            width: '614px',
-                            height: '440px',
-                            borderRadius: '100px 100px 100px 0px',
-                            padding: '50px 0px 50px 150px',
-                        }}>
-                            <p className="calistoga-regular mb-4" style={{color: 'black', fontSize: '2.5rem', lineHeight: 1.2}}>
-                                Sensasi <span style={{
-                                    color: '#800000'
-                                }}>kriuk</span> <br />
-                                yang <span style={{
-                                    color: '#800000'
-                                }}>luar biasa.</span>
-                            </p>
-                            <p style={{color: 'black', fontSize: '1.1rem', lineHeight: 1.2}}>
-                                Nikmati keripik singkong, talas, dan <br />
-                                pisang dengan rasa yang menggoda <br />
-                                dan kualitas terbaik. Setiap gigitan <br />
-                                memberikan kenikmatan yang tak <br />
-                                terlupakan!
-                            </p>
-                            <Link to="#" onClick={() => {
-                                const targetElement = document.getElementById('product-list');
-                                if (targetElement) {
-                                    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-                                }
-                            }}>
-                                <Button
-                                    type="secondary"
-                                    className="border-0 border-red-800 hover:border-red-600 hover:text-red-700 mt-20"
+                <div className="flex text-center w-full justify-evenly">
+                    <a href="" className="text-[#7B281D] hover:text-red-600 font-bold">Produk</a>
+                    <a href="" className="text-[#7B281D] hover:text-red-600 font-bold">Kategori</a>
+                </div>
+                <hr />
+                <div className="bg-white p-5">
+                    <Row gutter={[13, 13]}>
+                        {productsTerlaris.map((product) => (
+                            <Col span={6} key={product._id}>
+                                <Card
                                     style={{
-                                        backgroundColor: '#800000',
-                                        color: 'white',
-                                        borderRadius: '20px',
-                                        padding: '0px 15px 3px 15px',
-                                        height: '45px',
-                                        fontSize: '1.2rem',
-                                        fontWeight: '500'
+                                        height: '436px',
+                                        minHeight: '436px',
+                                        padding: 10,
                                     }}
-                                >
-                                    Beli Sekarang
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
+                                    hoverable
+                                    cover={
+                                        <img alt={product.name} className="border border-[#F2E8C6] p-2" src={product.thumbnail} style={{
+                                            minHeight: '250px',
+                                            maxHeight: '250px',
+                                            overflowY: 'hidden',
+                                            marginBottom: 'auto',
+                                            objectFit: 'cover',
+                                        }}/>
+                                    }>
+                                    <Card.Meta
+                                        style={{
+                                            marginTop: 'auto',
+                                            marginBottom: '3rem',
+                                        }}
+                                        title={product.name}
+                                        description={`Rp ${product.price}`}
+                                    />
+                                    
+                                    <div className="flex justify-between items-center">
+                                        <p>0 Terjual</p>
+                                        
+                                        <div className="flex flex-row-reverse">
+                                        <Link to={`/checkout/${product._id}`}>
+                                            <Button
+                                                type="primary"
+                                                icon={<ShoppingCartOutlined />}
+                                                className="bottom-0 ms-2 text-base"
+                                                // onClick={() => handleAddToCart(product)}
+                                            >
+                                            </Button>
+                                        </Link>
+                                        <Link to={`/`}>
+                                            <Button
+                                                type="primary"
+                                                danger
+                                                icon={<FaRegHeart />}
+                                                className="bottom-0 text-base"
+                                                // onClick={() => handleAddToCart(product)}
+                                            >
+                                            </Button>
+                                        </Link>
+                                        </div>
+                                    </div>
+                                    
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
                 </div>
             </div>
         </div>
