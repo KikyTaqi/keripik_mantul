bf976f1cfb5d8f1354c5f3aa1299f8bde0e51d72
KikyTaqi <rifqiramandhani3@gmail.com>
2025-01-20 09:22:02 +0700

    benerin desc

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "./src/**/*.{js, jsx, ts, tsx}", "./src/components/*.jsx", "./src/pages/*.jsx", "./src/pages/dashboard/*.jsx", "./src/pages/passwordChange/*.jsx", "./src/pages/About.jsx"
+    "./index.html", 
+    "./src/**/*.{js, jsx, ts, tsx}", 
+    "./src/components/*.jsx", 
+    "./src/pages/*.jsx", 
+    "./src/pages/dashboard/*.jsx", 
+    "./src/pages/passwordChange/*.jsx", 
+    "./src/pages/About.jsx"
   ],
   theme: {
     extend: {},
