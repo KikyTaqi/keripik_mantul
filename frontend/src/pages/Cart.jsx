27ed0857992ce78fd06a49c1063480ee276233e2
KikyTaqi <rifqiramandhani3@gmail.com>
2025-02-05 14:39:29 +0700

    cart final design

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -4,12 +4,14 @@ import axios from "axios";
 import { URL_PROFILE, URL_USER, URL_CART } from "../utils/Endpoint";
 import { jwtDecode } from "jwt-decode";
 import { icons } from "antd/es/image/PreviewGroup";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
   const [userId, setUserId] = useState("");
   const [selectedItems, setSelectedItems] = useState([]);
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchProfile();
@@ -62,119 +64,186 @@ const CartPage = () => {
           ? {
               ...item,
               quantity:
-                type === "increase" ? item.quantity + 1 : Math.max(1, item.quantity - 1),
+                type === "increase"
+                  ? item.quantity + 1
+                  : Math.max(1, item.quantity - 1),
             }
           : item
       )
     );
   };
 
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
-    <h2 className="text-2xl font-semibold mb-6 text-center">Keranjang Saya</h2>
-    <div className="mx-auto p-6 bg-white shadow-lg rounded-lg flex gap-5">
-      <div>
-
-      {cartItems.length === 0 ? (
-        <p className="text-center text-gray-500">Keranjang kosong</p>
-      ) : (
-        <div className="grid grid-cols-5 gap-4 text-center font-semibold border-b p-4 border-gray-300">
-          <div className="col-span-2">Produk</div>
-          <div>Harga</div>
-          <div>Jumlah</div>
-          <div>Aksi</div>
+      <div className="flex bg-[#F2E8C6] mb-3 p-3 py-5">
+        <button
+          onClick={handleBack}
+          className="text-4xl text-red-800 me-4 ms-3"
+        >
+          <IoArrowBackCircleOutline />
+        </button>
+        <div className="text-center w-full">
+          <h1 className="text-2xl font-semibold">Keranjang Saya</h1>
+          <h4>Lihat dan kelola produk yang sudah dipilih sebelum checkout</h4>
         </div>
-      )}
+      </div>
 
-      <div className="mt-4 space-y-4">
-        {cartItems.map((item) => (
-          <div
-            key={item.productId}
-            className="grid grid-cols-5 items-center gap-4 p-4 border border-gray-300 rounded-lg shadow-sm"
-          >
-            {/* Produk */}
-            <div className="col-span-2 flex items-center gap-4">
-              <img src={item.thumbnail} alt={item.name} className="w-16 h-16 object-cover rounded-lg" />
-              <span className="text-gray-700">{item.name}</span>
+      <div className="mx-auto p-6 bg-white grid grid-cols-6 gap-5">
+        <div className="col-span-4">
+          {cartItems.length === 0 ? (
+            <div className="border min-h-full rounded-md border-gray-300 flex items-center justify-center">
+              <p className="text-center text-gray-500">Keranjang kosong</p>
             </div>
-
-            {/* Harga */}
-            <div className="text-gray-700">Rp {item.price.toLocaleString("id-ID")}</div>
-
-            {/* Jumlah */}
-            <div className="flex items-center justify-center gap-2">
-              <button
-                className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
-                onClick={() => updateQuantity(item.productId, "decrease")}
-              >
-                <FaMinus />
-              </button>
-              <span className="px-3">{item.quantity}</span>
-              <button
-                className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
-                onClick={() => updateQuantity(item.productId, "increase")}
-              >
-                <FaPlus />
-              </button>
+          ) : (
+            <div className="grid grid-cols-6 gap-4 font-semibold border-b p-4 border-gray-300 text-base">
+              <div className="col-span-2">Produk</div>
+              <div>Harga</div>
+              <div>Jumlah</div>
+              <div>Total</div>
+              <div className="text-center">Aksi</div>
             </div>
+          )}
 
-            {/* Hapus */}
-            <div className="flex items-center justify-center gap-2">
-            <button
-                className={`w-6 h-6 flex items-center justify-center rounded-full border-2 transition-all duration-200 ${
-                  selectedItems.includes(item.productId)
-                    ? "bg-red-800 border-red-700 text-white"
-                    : "border-red-800 text-gray-400"
-                }`}
-                onClick={() => toggleSelect(item.productId)}
+          <div className="mt-4 space-y-4">
+            {cartItems.map((item) => (
+              <div
+                key={item.productId}
+                className="grid grid-cols-6 items-center gap-4 p-4 border border-gray-300 rounded-lg shadow-sm"
               >
-                <FaCheck
-                style={{ fontSize: "15px" }}
-                className={`transition-all duration-200 ${
-                selectedItems.includes(item.productId) ? "text-white" : "text-red-800"
-                }`}
-                />
-              </button>
-              <button
-                className="text-red-800 text-xl hover:text-red-700"
-                onClick={() => removeFromCart(item.productId)}
-              >
-                <FaTimesCircle style={{fontSize:"25px"}}/>
-              </button>
+                {/* Produk */}
+                <div className="col-span-2 flex items-center gap-4">
+                  <img
+                    src={item.thumbnail}
+                    alt={item.name}
+                    className="w-16 h-16 object-cover rounded-lg"
+                  />
+                  <span className="text-gray-700 text-sm">{item.name}</span>
+                </div>
+
+                {/* Harga */}
+                <div className="text-gray-700 text-base font-normal">
+                  Rp {item.price.toLocaleString("id-ID")}
+                </div>
+
+                {/* Jumlah */}
+                <div className="flex items-center bg-[#F2E8C6] gap-0 rounded-3xl w-max">
+                  <button
+                    className=" p-2 rounded-full hover:bg-[#F5E6B4]"
+                    onClick={() => updateQuantity(item.productId, "decrease")}
+                  >
+                    <FaMinus />
+                  </button>
+                  <span className="px-3 font-semibold">{item.quantity}</span>
+                  <button
+                    className=" p-2 rounded-full hover:bg-[#F5E6B4]"
+                    onClick={() => updateQuantity(item.productId, "increase")}
+                  >
+                    <FaPlus />
+                  </button>
+                </div>
+
+                {/* Total */}
+                <div className="text-gray-700 text-base font-semibold">
+                  Rp {(item.price * item.quantity).toLocaleString("id-ID")}
+                </div>
+
+                {/* Hapus */}
+                <div className="flex items-center justify-center gap-2">
+                  <button
+                    className={`w-6 h-6 flex items-center justify-center rounded-full border-2 transition-all duration-200 ${
+                      selectedItems.includes(item.productId)
+                        ? "bg-red-800 border-red-700 text-white"
+                        : "border-red-800 text-gray-400"
+                    }`}
+                    onClick={() => toggleSelect(item.productId)}
+                  >
+                    <FaCheck
+                      style={{ fontSize: "15px" }}
+                      className={`transition-all duration-200 ${
+                        selectedItems.includes(item.productId)
+                          ? "text-white"
+                          : "text-red-800"
+                      }`}
+                    />
+                  </button>
+                  <button
+                    className="text-red-800 text-xl hover:text-red-700"
+                    onClick={() => removeFromCart(item.productId)}
+                  >
+                    <FaTimesCircle style={{ fontSize: "25px" }} />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Ringkasan Pesanan */}
+        <div className="col-span-2">
+          <div className="border border-stone-300 rounded-md mb-3">
+            <div className="p-1 px-4">
+              <div className="text-center py-2">
+                <h1 className="font-bold text-lg text-red-800">
+                  Ringkasan Pesanan
+                </h1>
+              </div>
+              <hr className="border mb-2" />
+              <div className="grid grid-cols-3">
+                <div className="col-span-2">
+                  <p className="text-base font-normal">Subtotal</p>
+                </div>
+                <div className="">
+                  <p className="text-base font-normal">
+                    Rp{" "}
+                    {cartItems
+                      .filter((item) => selectedItems.includes(item.productId))
+                      .reduce(
+                        (acc, item) => acc + item.price * item.quantity,
+                        0
+                      )
+                      .toLocaleString("id-ID")}
+                  </p>
+                </div>
+              </div>
+              <div className="grid grid-cols-3">
+                <div className="col-span-2">
+                  <p className="text-base font-normal">Subtotal Pengiriman</p>
+                </div>
+                <div className="">
+                  <p className="text-base font-normal">Rp 0</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#F2E8C6] mt-6 p-3">
+              <div className="grid grid-cols-3">
+                <div className="col-span-2">
+                  <p className="text-base font-semibold">Total</p>
+                </div>
+                <div className="">
+                  <p className="text-base font-semibold">
+                    Rp{" "}
+                    {cartItems
+                      .filter((item) => selectedItems.includes(item.productId))
+                      .reduce(
+                        (acc, item) => acc + item.price * item.quantity,
+                        0
+                      )
+                      .toLocaleString("id-ID")}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        ))}
+          <button className="w-full mt-4 py-2 bg-red-800 text-white rounded-full font-semibold hover:bg-red-700">
+            CHECKOUT
+          </button>
+        </div>
+        
       </div>
-      </div>
-
-      {/* Ringkasan Pesanan */}
-      <div>
-      <div className="mt-6 rounded-lg shadow">
-        <h3 className="text-lg font-semibold border-b border-gray-300 px-2">Ringkasan Pesanan</h3>
-        <div className="flex justify-between mt-2 text-gray-700 px-2">
-          <span>Subtotal</span>
-          <span>
-            Rp{" "}
-            {cartItems
-              .filter((item) => selectedItems.includes(item.productId))
-              .reduce((acc, item) => acc + item.price * item.quantity, 0)
-              .toLocaleString("id-ID")}
-          </span>
-        </div>
-        <div className="flex justify-between mt-2 text-gray-700 px-2">
-          <span>Subtotal Pengiriman</span>
-          <span>Rp 0</span>
-        </div>
-        <div className="flex justify-between text-xl font-semibold bg-yellow-300 mt-2">
-          <span>Total</span>
-          <span>Rp {cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toLocaleString("id-ID")}</span>
-        </div>
-        </div >
-        <button className="w-full mt-4 py-2 bg-red-800 text-white rounded-full font-semibold hover:bg-red-700">
-          CHECKOUT
-        </button>
-        </div>
-    </div>
     </div>
   );
 };
